42c58987339a3272fd47c850be92872e49969baf
Joaquin <joaquinjrg01@gmail.com>
2024-01-08 19:19:31 +0100

    first commit

---
+++ Unidad4/Task1/joaquin_task_1.1.1.js.js
@@ -0,0 +1,20 @@
+//The second paragraph
+let secondPa = document.querySelector("p:nth-of-type(2)"); 
+
+//The title
+let title = document.getElementsByTagName("title");
+
+//The fist section
+let fistSection = document.querySelector("section");
+
+//All the paragraphs with class=parrafo_cuerpo
+let parrafoCuerpo = document.querySelectorAll("p.parrafo_cuerpo"); 
+
+//The "apellidos" input field
+let apellidos = document.getElementById("apellidos"); 
+
+//The last section
+let lastSection = document.querySelector("section:last-of-type"); 
+
+//All the paragraphs   
+let paragraphs = document.querySelectorAll("p")
