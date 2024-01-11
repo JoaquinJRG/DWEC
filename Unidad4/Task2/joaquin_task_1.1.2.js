d6f7101e5d3c502e62da0fc0d5f613b2d8c93d86
Joaquin <joaquinjrg01@gmail.com>
2024-01-11 15:43:24 +0100

    Ejercicio 1 y 2 tema 4

---
+++ Unidad4/Task2/joaquin_task_1.1.2.js
@@ -0,0 +1,21 @@
+//The table with id="age-table".
+let ageTable = document.getElementById("age-table"); 
+
+//All label elements inside that table (there should be 3 of them).
+let tableLabels =  ageTable.querySelectorAll("label"); 
+
+//The first td in that table (with the word “Age”).
+let ageTd = document.querySelector("[id*='age']"); 
+
+//The form with name="search".
+let form = document.querySelector("form[name='search']");
+
+
+//The first input in that form.
+let firstInput = form.querySelector("input[type='text']");
+console.log(firstInput);
+
+//The last input in that form.
+
+let lastInput = form.querySelector("input[type='submit']");
+console.log(lastInput);
