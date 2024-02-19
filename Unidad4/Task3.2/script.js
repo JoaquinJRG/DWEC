3ec01565e921055ad6915d6074e0eabeb06d2295
Joaquin <joaquinjrg01@gmail.com>
2024-02-19 19:13:07 +0100

    ejercicio 3.2

--- Unidad4/Task3.2/script.js
+++ Unidad4/Task3.2/script.js
@@ -1,13 +1,19 @@
-const URL = "https://www.mozilla.org"; 
+const URL = "https://www.mozilla.org";
 
-let newWindow = window.open(URL); 
+let newWindow = window.open(URL);
 
-
-setTimeout(function() {
-    newWindow.location.href = "https://www.google.es"; 
+setTimeout(function () {
+    newWindow.location.href = "https://www.google.es";
 }, 2000)
 
-
-setTimeout(function() {
-    newWindow.location.reload(true);
+setTimeout(function () {
+    newWindow.location.reload(true); 
 }, 4000)
+
+setTimeout(function () {
+    console.log("Protocol: " + location.protocol);
+    console.log("Hostname: " + location.hostname);
+    console.log("Host: " + location.host);
+    console.log("Port: " + location.port);
+}, 5000)
+
